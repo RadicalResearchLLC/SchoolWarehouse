c571780d0119877f212425a2882998728232d543
RadicalResearchLLC <MikeM@radicalresearch.llc>
2022-09-10 15:07:25 -0700

    first commit

---
+++ 1_Tidy.R
@@ -0,0 +1,37 @@
+## Data processing for school warehouse app
+## First created September 2022
+## Created by Mike McCarthy, PhD, Radical Research LLC
+
+library(sf)
+library(tidyverse)
+library(leaflet)
+library(htmltools)
+
+#Warehouse locations and polygons are from Warehouse CITY v1.08 @https://radicalresearch.shinyapps.io/WarehouseCITY/)
+
+#School locations and polygons are from the [California School Campus
+                                            #Database](https://www.californiaschoolcampusdatabase.org) Version 2021.
+
+wd <- getwd()
+school_wd <- paste0(wd, '/CSCD_2021.gdb')
+warehouse_wd <- paste0(wd, '/warehouse')
+app_wd <- paste0(wd, '/WarehouseCITY_school/')
+
+load(paste0(warehouse_wd, '/.RData'))
+
+counties <- c('Riverside', 'San Bernardino', 'Los Angeles')
+
+inland_parcels <- final_parcels %>% 
+  mutate(county = ifelse(county == 'San Bernadino', 'San Bernardino', county)) %>% 
+  filter(county %in% counties)
+
+rm(ls = final_parcels)
+
+schools <- sf::st_read(dsn = school_wd, quiet = TRUE, type = 3) %>%
+  filter(County %in% counties) %>% 
+  st_transform("+proj=longlat +ellps=WGS84 +datum=WGS84") %>% 
+  select(School, District, City, Status, GradesServed, Shape)
+
+setwd(app_wd)
+save.image('.RData')
+getwd()
